230375c84ea36f268e3eac2b688c586448a561c2
bviefhues <bernd.viefhues@web.de>
2022-09-01 15:42:28 +0200

    docstring updates

--- init.lua
+++ init.lua
@@ -3,12 +3,12 @@
 --- macOS application aware, keyboard-driven window switcher. Spoon 
 --- on top of Hammerspoon.
 ---
---- Cycles windows by focusing and raising them. All windows matching a 
+--- Switches windows by focusing and raising them. All windows matching a 
 --- bundelID, a list of bundleID's, an application name matchtext, 
---- or a list of application name matchtexts are cycled. Cycling applies 
---- to visible windows of currently focused space only. The spoon does 
---- not launch applications, it operates on open windows of running 
---- applications.
+--- or a list of application name matchtexts are switched by cycling 
+--- them. Cycling applies to visible windows of currently focused space 
+--- only. The spoon does not launch applications, it operates on open 
+--- windows of running applications.
 ---
 --- Example `~/.hammerspoon/init.lua` configuration:
 ---
